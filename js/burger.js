27b9cfa7676d311fc8f63ae1aff0bad9a044d258
Hashima <106578638+H4shima@users.noreply.github.com>
2023-04-03 13:55:55 +0300

    Delete burger.js

--- js/burger.js
+++
@@ -1,5 +0,0 @@
-hamburger = document.querySelector(".header__burger");
-hamburger.onclick = function() {
-  navBar = document.querySelector(".header__navbar");
-  navBar.classList.toggle("active");
-};
